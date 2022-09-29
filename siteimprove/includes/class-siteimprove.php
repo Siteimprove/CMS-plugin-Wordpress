cbcbc448583b95101b39d3f6e35744e4984eb6a4
Rodolfo Rodrigues <rudwolf@gmail.com>
2022-09-29 10:49:44 -0300

    small fix to overlay_dev.js path

--- siteimprove/includes/class-siteimprove.php
+++ siteimprove/includes/class-siteimprove.php
@@ -49,7 +49,7 @@ class Siteimprove {
 	protected $version;
 
 	const JS_LIBRARY_URL     = 'https://cdn.siteimprove.net/cms/overlay.js';
-	const JS_LIBRARY_DEV_URL = 'https://cdn.siteimprove.net/cms/overlay.js';
+	const JS_LIBRARY_DEV_URL = 'https://cdn.siteimprove.net/cms/overlay-dev.js';
 
 	/**
 	 * Define the core functionality of the plugin.
