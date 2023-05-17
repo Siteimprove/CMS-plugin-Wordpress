c83ecfafa445544ba7402915cacfdc94be6d67da
Jesper Nissen-Pedersen <jeppenp12@gmail.com>
2023-05-17 12:15:19 +0200

    Switch to Text Editor before doing firing off suggstions

--- siteimprove/admin/js/siteimprove.js
+++ siteimprove/admin/js/siteimprove.js
@@ -76,7 +76,6 @@
     common: function (url) {
       const _si = window._si || [];
       if (this.method == "contentcheck-flat-dom" || this.method == "contentcheck") {
-        console.log(this.domReference);
         _si.push([
           this.method,
           this.domReference,
@@ -88,10 +87,12 @@
       }
 
       var stack = [];
+      var usedSelectors = [];
 
       // Function to create an overlay at a specific position
-      function createOverlay(left, top, width, height) {
+      function createOverlay(left, top, width, height, id) {
         var overlay = document.createElement('div');
+        overlay.id = 'overlay-' + id;
         overlay.className = 'overlay';
         overlay.style.left = left + 'px';
         overlay.style.top = top + 'px';
@@ -101,19 +102,23 @@
         return overlay;
       }
 
+      function switchToTextEditor() {
+        if (typeof tinyMCE !== 'undefined') {
+          var editor = tinyMCE.activeEditor;
+          if (editor && !editor.isHidden()) {
+            switchEditors.go(editor.id, 'html');
+          }
+        }
+      }
+
       // Function to highlight all instances of a tag in a textarea
-      function highlightTagInTextarea(textarea, tag) {
+      function highlightTagInTextarea(textarea, tag, id) {
         // Get the textarea's position and line height
         var rect = textarea.getBoundingClientRect();
         var lineHeight = parseFloat(getComputedStyle(textarea).lineHeight);
 
         // Split the textarea's content into lines
         var lines = textarea.value.split('\n');
-
-        // Remove existing overlays
-        document.querySelectorAll('.overlay').forEach(function(overlay) {
-            overlay.remove();
-        });
 
         // Iterate over each line
         for (var i = 0; i < lines.length; i++) {
@@ -132,7 +137,7 @@
                         var height = (j - i + 1) * lineHeight;
 
                         // Create an overlay at this position
-                        createOverlay(left, top, width, height);
+                        createOverlay(left, top, width, height, id);
                         break;
                     }
                 }
@@ -140,8 +145,35 @@
         }
       }
 
+      function undo(textarea) {
+        var textarea = document.getElementById('content');
+        if (stack.length > 0) {
+            // Remove the last state from the stack and set it as the textarea value
+            var item = stack.pop();
+            usedSelectors.pop();
+            textarea.value = item.content;
+
+            var overlay = document.getElementById("overlay-" + item.id);
+            overlay.parentNode.removeChild(overlay);
+        }
+        if (stack.length === 0) {
+          // Get the siteimprove-notice element
+          var notice = document.getElementById('siteimprove-notice');
+  
+          // If the element exists, remove it
+          if (notice) {
+            notice.parentNode.removeChild(notice);
+          }
+        }
+      }  
+
+      var suggestionId = 0;
 
       _si.push(['onSuggestion', function(suggestionInfo) {
+        // Switch from Visual to Text Editor view
+        switchToTextEditor();
+
+        // TODO: Find a better way to figure out when the edit mode content starts
         var selector = suggestionInfo.locationSelector.replace("#document > body > div > main > div:nth-child(2) > div > ", "");
         var suggestedHtml = suggestionInfo.suggestedHtml;
       
@@ -151,58 +183,49 @@
         // Get the current content from the textarea
         var currentContent = textarea.value;
 
-        // Push the current content onto the stack
-        stack.push(currentContent);
-      
-        // Create a regular expression to match e.g. table tag
-        var regex = new RegExp('<' + selector + '[^>]*>[\\s\\S]*?</' + selector + '>', 'i');
-      
-        // Replace the table tag with the new HTML content
-        var updatedContent = currentContent.replace(regex, suggestedHtml);
-      
-        // Update the value of the textarea with the updated content
-        textarea.value = updatedContent;
+        if(!usedSelectors.includes(suggestionInfo.locationSelector)) {
+          // Scroll to top for overlays to work
+          window.scroll(0, 0);
+          // Push the current content onto the stack
+          stack.push({ content: currentContent, id: suggestionId });
+        
+          // Create a regular expression to match e.g. table tag
+          var regex = new RegExp('<' + selector + '[^>]*>[\\s\\S]*?</' + selector + '>', 'i');
+        
+          // Replace the table tag with the new HTML content
+          var updatedContent = currentContent.replace(regex, suggestedHtml);
+        
+          // Update the value of the textarea with the updated content
+          textarea.value = updatedContent;
 
-        if($('#siteimprove-notice').length === 0) {
-            // Add warning box
-            var infoBox = `
-            <div id="siteimprove-notice" class="notice notice-warning" style="display: block;">
-              <p class="siteimprove-notice" style="display: flex;align-items: center;">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false" aria-hidden="true" fill="currentColor" width="20px" height="20px"><path fill="#141155" d="M12.015625.113281C5.433594.113281.113281 5.433594.113281 12.015625c0 6.578125 5.320313 11.886719 11.902344 11.886719 6.578125 0 11.886719-5.324219 11.886719-11.886719 0-6.566406-5.324219-11.902344-11.886719-11.902344Zm0 0"></path><path fill="#fff" d="m6.097656 14.796875 1.695313-1.003906c.367187.945312 1.074219 1.539062 2.328125 1.539062 1.257812 0 1.625-.507812 1.625-1.074219 0-.746093-.679688-1.042968-2.1875-1.480468-1.539063-.4375-3.050782-1.074219-3.050782-3.007813 0-1.933593 1.609376-2.992187 3.332032-2.992187s2.9375.847656 3.613281 2.257812l-1.664063.960938c-.367187-.777344-.917968-1.300782-1.949218-1.300782-.832032 0-1.328125.4375-1.328125 1.019532 0 .621094.382812.945312 1.921875 1.410156 1.609375.523438 3.316406 1.058594 3.316406 3.121094 0 1.890625-1.523438 3.046875-3.671875 3.046875-2.058594.015625-3.441406-.972657-3.980469-2.496094m8.667969-6.917969c0-.621094.507813-1.160156 1.144531-1.160156.636719 0 1.15625.539062 1.15625 1.160156 0 .621094-.507812 1.140625-1.15625 1.140625-.648437 0-1.144531-.519531-1.144531-1.140625m.214844 1.988282h1.863281v7.230468h-1.863281Zm0 0"></path></svg> 
-                <strong style="margin-left: 5px;">Notification</strong>
-              </p>
-              <p class="help">
-                Siteimprove has added suggestion(s) to your Content. Please review them, you can click the button below to undo the applied suggestion
-              </p>
-              <p>
-                <button id="siteimprove-suggestion-undo" type="button" class="button">Undo the applied suggestion</button>
-              </p>
-            </div>
-            `;
-          $(infoBox).insertAfter('.wp-header-end');
+          // Push the selector to the usedSelectors array
+          usedSelectors.push(suggestionInfo.locationSelector);
+
+          if($('#siteimprove-notice').length === 0) {
+              // Add warning box
+              var infoBox = `
+              <div id="siteimprove-notice" class="notice notice-warning" style="display: block;">
+                <p class="siteimprove-notice" style="display: flex;align-items: center;">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false" aria-hidden="true" fill="currentColor" width="20px" height="20px"><path fill="#141155" d="M12.015625.113281C5.433594.113281.113281 5.433594.113281 12.015625c0 6.578125 5.320313 11.886719 11.902344 11.886719 6.578125 0 11.886719-5.324219 11.886719-11.886719 0-6.566406-5.324219-11.902344-11.886719-11.902344Zm0 0"></path><path fill="#fff" d="m6.097656 14.796875 1.695313-1.003906c.367187.945312 1.074219 1.539062 2.328125 1.539062 1.257812 0 1.625-.507812 1.625-1.074219 0-.746093-.679688-1.042968-2.1875-1.480468-1.539063-.4375-3.050782-1.074219-3.050782-3.007813 0-1.933593 1.609376-2.992187 3.332032-2.992187s2.9375.847656 3.613281 2.257812l-1.664063.960938c-.367187-.777344-.917968-1.300782-1.949218-1.300782-.832032 0-1.328125.4375-1.328125 1.019532 0 .621094.382812.945312 1.921875 1.410156 1.609375.523438 3.316406 1.058594 3.316406 3.121094 0 1.890625-1.523438 3.046875-3.671875 3.046875-2.058594.015625-3.441406-.972657-3.980469-2.496094m8.667969-6.917969c0-.621094.507813-1.160156 1.144531-1.160156.636719 0 1.15625.539062 1.15625 1.160156 0 .621094-.507812 1.140625-1.15625 1.140625-.648437 0-1.144531-.519531-1.144531-1.140625m.214844 1.988282h1.863281v7.230468h-1.863281Zm0 0"></path></svg> 
+                  <strong style="margin-left: 5px;">Notification</strong>
+                </p>
+                <p class="help">
+                  Siteimprove has applied suggestion(s) to your content. If you wish to undo these changes, you can press the button below
+                </p>
+                <p>
+                  <button id="siteimprove-suggestion-undo" type="button" class="button">Undo the applied suggestion</button>
+                </p>
+              </div>
+              `;
+            $(infoBox).insertAfter('.wp-header-end');
+          }
+          var undoButton = document.getElementById('siteimprove-suggestion-undo');  
+          // Add overlay
+          highlightTagInTextarea(textarea, selector, suggestionId);
+          suggestionId++;
+
+          undoButton.addEventListener('click', undo);
         }
-        var undoButton = document.getElementById('siteimprove-suggestion-undo');
-        function undo() {
-          if (stack.length > 0) {
-              // Remove the last state from the stack and set it as the textarea value
-              textarea.value = stack.pop();
-              var overlay = document.querySelector(".overlay");
-              overlay.parentNode.removeChild(overlay);
-          }
-          if (stack.length === 0) {
-            // Get the siteimprove-notice element
-            var notice = document.getElementById('siteimprove-notice');
-    
-            // If the element exists, remove it
-            if (notice) {
-              notice.parentNode.removeChild(notice);
-            }
-          }
-        }    
-        // Add overlay
-        highlightTagInTextarea(textarea, selector);
-
-        undoButton.addEventListener('click', undo);
       }]);
                 
 
