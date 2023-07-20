3bb2cfa2dc592c07524efeaf9cac74c1f0e88106
Jesper Nissen-Pedersen <jeppenp12@gmail.com>
2023-07-20 10:54:53 +0200

    INS-2791: Add it as an else instead of checking si_preview

--- siteimprove/includes/class-siteimprove.php
+++ siteimprove/includes/class-siteimprove.php
@@ -148,8 +148,7 @@ class Siteimprove {
 		$this->loader->add_action( 'admin_init', $plugin_admin, 'siteimprove_init' );
 		if ( isset( $_GET['si_preview'] ) || '1' === $_GET['si_preview'] ) {
 			$this->loader->add_action( 'wp_head', $plugin_admin, 'siteimprove_preview' );
-		}
-		if ( ! isset( $_GET['si_preview'] ) || '0' === $_GET['si_preview'] ) {
+		} else {
 			$this->loader->add_action( 'publish_page', $plugin_admin, 'siteimprove_save_session_url_post' );
 			$this->loader->add_action( 'publish_post', $plugin_admin, 'siteimprove_save_session_url_post' );
 			$this->loader->add_action( 'edit_term', $plugin_admin, 'siteimprove_save_session_url_term', 10, 3 );
