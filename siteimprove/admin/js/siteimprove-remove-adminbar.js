9cbd715a91614fd01cb5068e229fbcddc41fbd11
Jesper Nissen-Pedersen <jeppenp12@gmail.com>
2023-07-20 10:04:21 +0200

    INS-2791: Change onload event

--- siteimprove/admin/js/siteimprove-remove-adminbar.js
+++ siteimprove/admin/js/siteimprove-remove-adminbar.js
@@ -1,7 +1,7 @@
-window.onload = function() {
+window.addEventListener("load", function() {
     var adminBar = document.getElementById('wpadminbar');
     if (adminBar) {
         adminBar.innerHTML = '<div></div>';
         adminBar.id = 'wpadminbar-disabled';
     }
-};
+});
