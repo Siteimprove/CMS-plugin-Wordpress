ace98be3f9d0b86ccda0ab4bf64fb0e01d1867dc
Jesper Nissen-Pedersen <jeppenp12@gmail.com>
2023-08-23 15:18:08 +0200

    Revert back to 2.0.4

--- siteimprove/siteimprove.php
+++ siteimprove/siteimprove.php
@@ -9,7 +9,7 @@
  * Plugin Name:         Siteimprove Plugin
  * Plugin URI:          https://www.siteimprove.com/integrations/cms-plugin/wordpress/
  * Description:         Integration with Siteimprove.
- * Version:             2.0.5
+ * Version:             2.0.4
  * Author:              Siteimprove
  * Author URI:          http://www.siteimprove.com/
  * Requires at least:   4.7.2
